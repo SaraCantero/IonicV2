ef0cad5d10556d8fe0978a5df59a5d12aaa918da
Sara Cantero <cantero.mesar20@cadiz.salesianos.edu>
2021-02-10 11:35:16 +0100

    sara

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -30,7 +30,7 @@ export class LoginPage implements OnInit {
       //Para obtener datos del usuario que se loguea
       this.restService.setToken(this.token.data.token);
       this.restService.setcicleUser(this.token.data.cicle_id);
-      this.restService.setActUser(this.token.data.actived)
+      
       //
       if(this.token.success){
         console.log('Login correcto');
