4a06849af3d48b3e9d37b5d76ca598231efeec84
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-02-14 21:59:18 +0100

    j

--- src/app/pages/client/tab3/tab3.page.ts
+++ src/app/pages/client/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RestService } from 'src/app/services/rest.service';
 import { OffersData } from 'src/app/interfaces/offers';
+import { Tab2Page } from '../tab2/tab2.page';
 
 @Component({
   selector: 'app-tab3',
@@ -10,10 +11,18 @@ import { OffersData } from 'src/app/interfaces/offers';
 export class Tab3Page {
   
   offersApliques: OffersData[]=[];
+  images=[];
+  contador=0;
+  contador2=0;
+  cicles:any;
 
   constructor(public restService: RestService) {
 
+    this.obtenerCicloIds();
     this.obtenerOfertasApliques();
+    setTimeout(() => {
+      this.offerYimg();
+    },1000)
   }
 
   obtenerOfertasApliques(){
@@ -26,8 +35,44 @@ export class Tab3Page {
     );
   }
 
-  abrirModalApliques($id2: any, $titulo2: any, $descripcion2: any, $ciclo2: any, 
+  abrirModalApliques($image:any, $id2: any, $titulo2: any, $descripcion2: any, $ciclo2: any, 
     $fecha2: any, $cand2: any){
-    this.restService.abrirModal($id2, $titulo2, $descripcion2, $ciclo2, $fecha2, $cand2);
+    this.restService.abrirModal($image, $id2, $titulo2, $descripcion2, $ciclo2, $fecha2, $cand2);
   }
+
+  offerYimg(){
+    console.log("ENTRO FOTOS");
+    this.contador=0;
+    this.images=[];
+    console.log("OFFERS:")
+    console.log(this.offersApliques);
+    console.log("CICLES:");
+    console.log(this.cicles);
+    this.offersApliques.forEach((f: {cicle_id: number}) => {
+      console.log("FOREACH");
+      this.cicles.forEach((c: {id: number, img: any}) => {
+        console.log("FOREACHDOBLE");
+        if(f.cicle_id==c.id){
+          this.images[this.contador]=c.img;
+          this.contador=this.contador+1;
+        }
+      })
+    })
+    console.log("HEY");
+    console.log(this.images);
+    console.log("HEY");
+    this.restService.setImagesOffers(this.images);
+  }
+
+  obtenerCicloIds(){
+    console.log("ENTRO CICLOS");
+    this.contador2=0;
+    this.restService.getCicles()
+    .then((res: any) => {
+      console.log("OBTENGO CICLOS");
+      this.cicles=res.data;
+      console.log(this.cicles);
+    })
+  }
+
 }
--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -21,13 +21,14 @@ export class RestService {
   //cicleUser: any;
   cicleIdFoto: any;
   type="client";
+  imagesOffers=[];
+  cicles: any;
 
   //Temporales
   tokenLogin = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiODY4ZDQ0MzgwNThkZDVlYzM3YzFhNzBlZmFjOTAxY2FhZTk5NjJmNzBlMTM4ODJmZGUzNTZkZjkzNDVhMjI5MzE0OTQwNDhlODdhZmIwZjMiLCJpYXQiOjE2MTMwMzA4MzQsIm5iZiI6MTYxMzAzMDgzNCwiZXhwIjoxNjQ0NTY2ODM0LCJzdWIiOiIzNyIsInNjb3BlcyI6W119.ikS3Css9uviVgfGW9vqO9X3w1fei42T6BmU7p6pzNK0NRZoaXhKZNW-Vbmiw2ACgBV78D5e0rXBapWJHVa_RWmV2KqTv7u1bAhpug3ktO4THzdml_phoxy9QydsdrOHE60p4Qxs3oY8exPM2IJ0X65EFiS7PkB33hrc9P2YSVtqVX5_6CReZ0xeOZhZlRxb12CMOR_YMCoVSR-e4xwW5IfMADTT8LoUyEeCHYWW8QkhiAvBgeY8nhJFhqMD5i6BWFJU4oWPOFbpKfDxbnjE26B4nyNxGBc0YKfYxOigFe9TbY1NXCs6xvCpgpE0hFXy7Ywl02C2i042c5_oq1wMdxGiw6ql_QuYLIN1NBE3FNHsG0-Z09F0uu8Vu_fnnHqCrhW4bftdspx61HvWbW8T7WZgUYr9tXwLJCO9hQ7dGDzqWAsV9AUMlZcJDsTMaQLSCC4xWibiFv6Pa7HxXZkK41x3OutkBHhUNJtF0uHrFyNuTE_nRIxn7e1V8cZw7V6FOk8iDRKZx9pMpIBtD4Ogq54QcDCW3d3xsSvHO_JF2GSliVjfqeJzayezjFfgvK05Iet4xvLjQ6Jqo0wA8UTxT4x17hM8krXavLWi_Cij0jJI9m1o2_-wpAk5Vwm7G-TbiY2A5KKJnLiOcQcdqN5kZ5rKJf2L8XQxiK0jT9R1w_RE";
   token2 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMDUzYTFhOTNmNjMyOGE4ZWJjY2ZlYzZkMGE0YzgwZmE1M2RhYzBiNzgzOGI4NmVjZmE1NGU4YzZmOWIwM2ZkZDI4ZDFmMGM4MmVlYzZlMTciLCJpYXQiOjE2MTMwNDEwMTQsIm5iZiI6MTYxMzA0MTAxNCwiZXhwIjoxNjQ0NTc3MDE0LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.g3ouhEO3AGccEQxQdAhWy67JDfVXL1NbPDgvnT75TfuUQBCMYvyo3l3i1J1dzekfzOI9-GCgmS5EtZkINK4NvKp2FVIEjpAli9XJGd5Fnbr8cqN823NFMMBkCGdohGAFNkOo03jOgeX7ubg4NYRsF2R-h59UlKkaUVeDayezKId3pYQ_OE1snZugT5Gp7TMaK9KlDJlbC5z04qY6I_QL5CeTMETv8GjjViRPbcL4l3faC8dpIC7hbh-kyblsXD2lk60bU0ByfGZkEMxJjR_Obbh_2TqIXY19d4PZRU7qQOXfHgcwxF9ftPAWK_piAxKkwpJZnWE1HCFp3AWqdNlh2PphFyUvwqT5sj_TzX_-JViFhFhRrQ6bvqiiiyykw4OwmHVsNLgXtZCfWxUIAuM3OA8QHXxisMeMzJx0HW3wEKV3dE2DzJ9dnV7JBET8t0To5tXNm1wIxBsPA2cQtsQ9uexC2JAtBUBzIQn8Hm7IJGfWJ-i-VA09EUV6ObAgDKd0EKVmHyv5e1iPr3aCXzPEYOojOY_8QNH4jHeDOSnl_avInuxhIn9QEVcP0eP5GW6YDwMwBBYfOY_V2knjChvI3Egai6viMfAhhmuP2aRQa5tDAl4iIRh_Fa60o53Swl0vHnSut3fKlKIZRMfHprZi95nT_xu6JKb6hiexO1BNtcw";
   cicleUser=1;
   userId=37;
-  imagesOffers=[];
   //
  
 
@@ -158,10 +159,11 @@ export class RestService {
     });
   }
 
-  async abrirModal($id: any, $titulo: any, $descripcion: any, $ciclo: any, $fecha: any, $cand: any){
+  async abrirModal($image:any, $id: any, $titulo: any, $descripcion: any, $ciclo: any, $fecha: any, $cand: any){
     const modal = await this.modalCtrl.create({
       component: OfferComponent,
       componentProps: {
+      imageCicle: $image,
       headline: $titulo,
       description: $descripcion,
       id: 'Id: '+$id,
@@ -309,5 +311,4 @@ export class RestService {
   getImagesOffers(){
     return this.imagesOffers;
   }
-
 }
